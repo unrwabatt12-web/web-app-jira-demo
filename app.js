e5e91a68ae533130e120b4f928476248d4857030
KALISA <unrwabatt12@gmail.com>
2025-11-15 19:37:39 +0200

    WEB-1: Add homepage route with welcome message

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello from Jira Demo App!");
+//app.get("/", (req, res) => {
+//    res.send("Hello from Jira Demo App!");
+//
+//});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Homepage!');
 });
 
 app.listen(PORT, () => {
