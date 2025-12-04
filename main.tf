a71930e45607d2a57f3c4ec119bd3a034c3e570a
KALISA <unrwabatt12@gmail.com>
2025-12-04 22:11:42 +0200

    WEB-1: Initial Terraform local webapp setup

---
+++ main.tf
@@ -0,0 +1,45 @@
+##########################################
+# 1. Terraform Provider Configuration
+##########################################
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
+
+##########################################
+# 2. Local Variable (Node.js App Code)
+##########################################
+locals {
+  app_code = <<EOF
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Terraform Local Lab!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+EOF
+}
+
+##########################################
+# 3. Create index.js in webapp/src/
+##########################################
+resource "local_file" "webapp_app" {
+  filename = "${path.module}/webapps/src/index.js"
+  content  = local.app_code
+}
+
+##########################################
+# 4. Output the File Path
+##########################################
+output "webapp_file_path" {
+  value       = local_file.webapp_app.filename
+  description = "The full path to the web application's index.js file."
+}
